444de08e328f88bd849a4e5fe2487fbf22cab8e0
Daniel Silva <danielzgsilva@knights.ucf.edu>
2021-02-02 21:09:53 -0500

    fixed accuracy bug

--- validation.py
+++ validation.py
@@ -8,6 +8,7 @@ from torchvision import datasets, transforms, models
 
 from pgd import PGD
 
+
 def main(experiment, apply_pgd=False):
     model = None
     if experiment == 'resnet':
@@ -15,53 +16,47 @@ def main(experiment, apply_pgd=False):
     elif experiment == 'vgg':
         model = models.vgg16(pretrained=True)
 
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-    imagenet_data = datasets.ImageFolder('Imagenet/validation/',
-                                         transforms.Compose([
-                                             transforms.Resize(256),
-                                             transforms.CenterCrop(224),
-                                             transforms.ToTensor(),
-                                             normalize]))
+    imagenet_data = datasets.ImageFolder('~/Imagenet/validation/',
+                                      transforms.Compose([
+                                      transforms.Resize(256),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      normalize]))
 
     data_loader = torch.utils.data.DataLoader(imagenet_data,
                                               batch_size=24,
                                               shuffle=True)  # TODO num_workers for multi-GPU
 
-
-    accuracy = validation(model, data_loader, apply_pgd)
+    accuracy = validation(model, data_loader, apply_pgd, device)
     print('Accuracy: ', accuracy * 100)
 
 
-def validation(model, data_loader, apply_pgd):
+def validation(model, data_loader, apply_pgd, device):
     if model is None:
         return
 
-    model.cuda()
+    model.to(device)
     model.eval()
 
     pgd_attack = PGD(model, 5, 1, 2)
     num_correct = 0
 
     for images, labels in tqdm(data_loader):
-        images.cuda()
-        labels.cuda()
+        images = images.to(device)
+        labels = labels.to(device)
         if apply_pgd:
             images = pgd_attack(images, labels)
 
         with torch.no_grad():
-            print(next(model.parameters()).is_cuda)
-            print(images.get_device())
-            print(labels.get_device())
             outputs = model(images)
             predictions = torch.argmax(outputs.data, 1)
-            num_correct += (predictions == labels).sum()
-            # equality = (labels.data == prediction)
-            # accuracy += equality.type(torch.FloatTensor).mean()
+            num_correct += (predictions == labels).sum().item()
 
-    print('correct', num_correct)
-    print('total', len(data_loader.dataset))
     return num_correct / len(data_loader.dataset)
 
 
